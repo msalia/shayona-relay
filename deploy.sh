7169f82f8a2b09fcff520aae273962767f157212
Mukund Salia <salia.mukund@gmail.com>
2026-01-31 19:13:46 -0500

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # remove old container and dependencies
 docker compose down
-docker image rm shayona-masi-api
+docker image rm shayona-relay
 
 # start a new container
 docker compose -f docker-compose.prod.yaml up -d
