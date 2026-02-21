bdb444ff78043e694307502bcf40d8e5d5a9dd47
Mukund Salia <salia.mukund@gmail.com>
2026-02-21 16:49:03 -0500

    update build for windows

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Build for Windows
 
-> PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
+> export PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
 > mysql --version
 > cargo build --target x86_64-pc-windows-gnu
--- build_windows.sh
+++ build_windows.sh
@@ -1,3 +1,5 @@
-PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
+export MYSQLCLIENT_CFLAGS=`pkg-config mysqlclient --cflags`
+export MYSQLCLIENT_LDFLAGS=`pkg-config mysqlclient --libs`
+export PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
 mysql --version
 cargo build --target x86_64-pc-windows-gnu
