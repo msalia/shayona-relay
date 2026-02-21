575309931c09b5923ca7391c23352908a1eabee6
Mukund Salia <salia.mukund@gmail.com>
2026-02-21 17:24:30 -0500

    fix bat

--- publish/start_relay.bat
+++ publish/start_relay.bat
@@ -1,9 +1,4 @@
-@echo off
-REM Set environment variables temporarily
-set OB_URL="https://10.1.3.130/app/org.openbravo.mobile.core.service.jsonrest/org.openbravo.retail.posterminal.ExternalOrderLoader?user=microsexport&password=openbravo&synchronizedProcessing=true"
-set DATABASE_URL="mysql://datauser:Mr\$0200097a@localhost:3306/checkpostingdb"
-set HOST=0.0.0.0
-set PORT=3002
-
 REM Run your executable
 shayona-kiosk-relay.exe
+
+cmd /k
